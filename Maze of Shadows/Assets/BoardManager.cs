26d95d72dff9b95b9a8b3f3f8364c482742d055f
Logan Falkenberg <loganf0101@gmail.com>
2025-03-12 21:22:38 -0500

    making some progress on the board

---
+++ Maze of Shadows/Assets/BoardManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public GameObject tilePrefab;
+    public int boardSize = 5;       // 5x5
+    public float tileSize = 1.0f;   // How large each tile is in Unity units
+
+    [HideInInspector] public GameObject[,] board;    // 2D array of all tiles
+    [HideInInspector] public Vector2Int emptySpot;   // The (x,y) of our empty tile
+
+    void Start()
+    {
+        board = new GameObject[boardSize, boardSize];
+        int totalTiles = boardSize * boardSize - 1; // 24
+        int createdTiles = 0;
+
+        for (int y = 0; y < boardSize; y++)
+        {
+            for (int x = 0; x < boardSize; x++)
+            {
+                if (createdTiles < totalTiles)
+                {
+                    // Create a tile at (x, y) in world space
+                    Vector3 spawnPos = new Vector3(x * tileSize, y * tileSize, 0f);
+                    GameObject tileObj = Instantiate(tilePrefab, spawnPos, Quaternion.identity);
+                    tileObj.transform.SetParent(transform);
+
+                    // Set up the tile's script
+                    TileController tile = tileObj.GetComponent<TileController>();
+                    tile.boardManager = this;
+                    tile.x = x;
+                    tile.y = y;
+
+                    board[x, y] = tileObj;
+                    createdTiles++;
+                }
+                else
+                {
+                    // The final spot is empty
+                    board[x, y] = null;
+                    emptySpot = new Vector2Int(x, y);
+                }
+            }
+        }
+    }
+}
--- Maze of Shadows/Assets/TileController.cs
+++ Maze of Shadows/Assets/TileController.cs
@@ -1,27 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TileController : MonoBehaviour
 {
-    public float stepSize = 1f;
+    public BoardManager boardManager;
+    public int x; // Board coordinate
+    public int y; // Board coordinate
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-            transform.position += new Vector3(0, stepSize, 0);
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-            transform.position += new Vector3(0, -stepSize, 0);
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-            transform.position += new Vector3(-stepSize, 0, 0);
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-            transform.position += new Vector3(stepSize, 0, 0);
+        // If we're adjacent to the empty spot, allow a move
+        if (IsAdjacentToEmpty())
+        {
+            // If the empty spot is directly above us
+            if (Input.GetKeyDown(KeyCode.UpArrow) && boardManager.emptySpot.y == y + 1)
+            {
+                SwapWithEmpty();
+            }
+            // If it's directly below
+            if (Input.GetKeyDown(KeyCode.DownArrow) && boardManager.emptySpot.y == y - 1)
+            {
+                SwapWithEmpty();
+            }
+            // If it's directly left
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && boardManager.emptySpot.x == x - 1)
+            {
+                SwapWithEmpty();
+            }
+            // If it's directly right
+            if (Input.GetKeyDown(KeyCode.RightArrow) && boardManager.emptySpot.x == x + 1)
+            {
+                SwapWithEmpty();
+            }
+        }
+    }
+
+    bool IsAdjacentToEmpty()
+    {
+        Vector2Int e = boardManager.emptySpot;
+        // Adjacent if difference in X is 1 (same Y) or difference in Y is 1 (same X)
+        bool adjacentX = (Mathf.Abs(e.x - x) == 1 && e.y == y);
+        bool adjacentY = (Mathf.Abs(e.y - y) == 1 && e.x == x);
+        return adjacentX || adjacentY;
+    }
+
+    void SwapWithEmpty()
+    {
+        // 1. Store the tile’s old position
+        int oldX = x;
+        int oldY = y;
+
+        // 2. Grab the empty spot
+        Vector2Int e = boardManager.emptySpot;
+        int emptyX = e.x;
+        int emptyY = e.y;
+
+        // 3. The tile now goes to the empty spot
+        boardManager.board[emptyX, emptyY] = gameObject; 
+        x = emptyX;
+        y = emptyY;
+
+        // 4. The tile’s old position becomes the new empty spot
+        boardManager.board[oldX, oldY] = null;
+        boardManager.emptySpot = new Vector2Int(oldX, oldY);
+
+        // 5. Visually update the tile’s transform
+        transform.position = new Vector3(x * boardManager.tileSize,
+                                         y * boardManager.tileSize,
+                                         0f);
     }
 }
