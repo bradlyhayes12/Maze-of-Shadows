82174bdb9af6b1ba15719eb682c1a4a2ca23a22c
Logan Falkenberg <loganf0101@gmail.com>
2025-03-12 19:06:36 -0500

    Updated the manager with scripts to load the correct scenes

---
+++ Maze of Shadows/Assets/ViewManagerScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ViewManagerScript : MonoBehaviour
+{
+     public void LoadScene(string sceneName) {
+        // Debug.Log("Loading scene: " + sceneName);
+        SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+    }
+
+    public void UnloadScene(string sceneName) {
+        SceneManager.UnloadSceneAsync(sceneName);
+    }
+}
