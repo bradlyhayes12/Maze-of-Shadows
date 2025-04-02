04c82e8ab97292a459aee571030ec065c7875a60
Brad Hayes <149333599+bradlyhayes12@users.noreply.github.com>
2025-04-02 16:54:17 -0500

    Fire wizard has a hitbox with the sword

--- Maze of Shadows/Assets/Characters/Fire Wizard/FireWizard.cs
+++ Maze of Shadows/Assets/Characters/Fire Wizard/FireWizard.cs
@@ -8,20 +8,24 @@ public class FireWizard : MonoBehaviour
     private Animator animator;
     private CharacterMovement movementScript;  // Updated this line!
 
+    [Header("Fireball Attack")]
     public float fireballBurstTime = 1f;
     public float fireballCooldown = 5f;
-
     private bool canFire = true;
 
+    [Header("Melee Attack")]
     public float attackDuration = 1f; // Length of the melee animation
     public float attackCoolDown = 2f;
     private bool isAttacking = false;
+
+    public MeleeHitbox swordHitbox;
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
         movementScript = GetComponent<CharacterMovement>(); // get movement script
+
     }
 
     void Update()
@@ -48,9 +52,15 @@ public class FireWizard : MonoBehaviour
         if (animator != null)
             animator.SetBool("isMoving", false);
 
+        if (swordHitbox != null)
+            swordHitbox.EnableHitbox();
+
         animator.SetTrigger("Melee");
 
         yield return new WaitForSeconds(attackDuration);
+
+        if (swordHitbox != null)
+            swordHitbox.DisableHitbox();
 
         if (movementScript != null)
             movementScript.enabled = true;
